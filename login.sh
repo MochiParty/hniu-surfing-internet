c6792eb618afeaa254ac398712a51aa98af03ae0
Ayou <31991515+Ayouuuu@users.noreply.github.com>
2022-05-16 13:04:13 +0800

    fix success login bug

--- login.sh
+++ login.sh
@@ -3,13 +3,17 @@ datetime=$(date +[%Y-%m-%d_%H:%M:%S])
 # 账号 密码
 user=""
 pwd=""
-
-checkCode=`curl -o /dev/null -s -w %{http_code} baidu.com`;
+check(){
+    return `curl -o /dev/null -s -w %{http_code} baidu.com`
+}
+code
+checkCode=$(echo $?)
 
 if [[ "$checkCode" != "200" ]]; then
     content=`curl -m 10 -s -w "Code: %{http_code}\n" -d "DDDDD=$user&upass=$pwd&0MKKey=123456" http://10.253.0.1/a70.html`;
-	title=`echo "$content" | grep '<title>(.*)</title>' -E`
-	if [[ ${#title} == 22 ]]; then
+    code
+    checkCode=$(echo $?)
+	if [[ "$checkCode" != "200" ]]; then
 		msg="$datetime Successfully login,code: $code title: $title"
 		echo $msg
 		echo $msg >> "$BASE_PATH/log.log"
